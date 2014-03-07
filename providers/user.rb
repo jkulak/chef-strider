5b9aa4cbadd0375cd87b1f18844bcc4584a726a8
Stafford Brunk <wingrunr21@gmail.com>
2014-03-07 14:58:26 -0700

    Make sure the db_uri in the user LWRP doesn't start with mongo://

--- providers/user.rb
+++ providers/user.rb
@@ -4,8 +4,9 @@ action :add do
   email = new_resource.email
   password = new_resource.password
   admin = new_resource.admin
+  db_uri = new_resource.server.gsub('mongodb://', '')
   execute "add Strider user #{email}" do
     command "strider addUser -l #{email} -p #{password} -a #{admin}"
-    only_if "mongo #{new_resource.server} --eval 'printjson(db.users.find({email:\"#{email}\"}).count())' --quiet | grep 0"
+    only_if "mongo #{db_uri} --eval 'printjson(db.users.find({email:\"#{email}\"}).count())' --quiet | grep 0"
   end
 end
